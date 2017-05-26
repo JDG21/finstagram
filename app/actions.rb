3bd42b55a4da74ecddee4d3b2ac617d8aa5a095b
James De Grano <jamesdegrano1@gmail.com>
2017-05-26 03:06:48 +0000

    V1.26 More ruby code added

--- app/actions.rb
+++ app/actions.rb
@@ -7,7 +7,7 @@ def humanized_time_ago(minute_num)
 end
 
 get '/' do
-        post = {
+        post_WannaCry = {
     username: "WannaCry_2017",
     avatar_url: "https://www.nbs-system.com/wp-content/uploads/white-hats.png",
     photo_url: "https://www.troyhunt.com/content/images/2017/05/WannaCrypt.jpg",
@@ -19,7 +19,7 @@ get '/' do
         text:  "Just got Hacked over the weekend"
         }]
     }
-        post = {
+        post_ABT = {
     username: "ABT_2017",
     avatar_url: "https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAJTAAAAJGMzOGVhMmY0LWUwNzUtNGI3YS05MGU0LWE3NDU4YWI2NTI4ZQ.png",
     photo_url: "https://www.absolute.com/-/media/Commercial/images/icons/linear/billion-endpoints.png?la=en&hash=FC47FEACEFCE6CB3501CFCF0C7A7E57B7AFB5B27",
@@ -31,7 +31,7 @@ get '/' do
         text:  "You should of had Absolute"
         }]
     }
-        post = {
+        post_Sophos = {
     username: "Sophos_2017",
     avatar_url: "https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAXlAAAAJDA5ZTQzN2YxLTFhNWItNDQzYy04OWY2LWZiMzZmMDQ0OWNhMg.png",
     photo_url: "http://www.ft.com/fastft/files/2017/05/sophos-attack.png",
@@ -43,4 +43,5 @@ get '/' do
         text:  "Stocks going up, weehoooo"
         }]
     }
+    [post_WannaCry, post_ABT, post_Sophos].to_s
 end
