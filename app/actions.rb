053ec72fdbf310be7c3aca90c64de417ea30f383
James De Grano <jamesdegrano1@gmail.com>
2017-06-07 02:31:46 +0000

    V1.51 made it dynamic

--- app/actions.rb
+++ app/actions.rb
@@ -1,49 +1,7 @@
-def humanized_time_ago(minute_num)
-    if minute_num >= 60
-        "#{minute_num / 60}h"
-    else 
-        "#{minute_num}m"
-    end
-end
+
 
 get '/' do
-        @post_WannaCry = {
-    username: "WannaCry_2017",
-    avatar_url: "https://www.nbs-system.com/wp-content/uploads/white-hats.png",
-    photo_url: "https://www.troyhunt.com/content/images/2017/05/WannaCrypt.jpg",
-    humanized_time_ago: humanized_time_ago(1),
-    like_count: 0,
-    comment_count: 1,
-    comments: [{ 
-        username: "WannaCry_2017",
-        text:  "Just got Hacked over the weekend"
-        }]
-    }
-        @post_ABT = {
-    username: "ABT_2017",
-    avatar_url: "https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAJTAAAAJGMzOGVhMmY0LWUwNzUtNGI3YS05MGU0LWE3NDU4YWI2NTI4ZQ.png",
-    photo_url: "https://www.absolute.com/-/media/Commercial/images/icons/linear/billion-endpoints.png?la=en&hash=FC47FEACEFCE6CB3501CFCF0C7A7E57B7AFB5B27",
-    humanized_time_ago: humanized_time_ago(120),
-    like_count: 0,
-    comment_count: 1,
-    comments: [{ 
-        username: "ABT_2017",
-        text:  "You should of had Absolute"
-        }]
-    }
-        @post_Sophos = {
-    username: "Sophos_2017",
-    avatar_url: "https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAXlAAAAJDA5ZTQzN2YxLTFhNWItNDQzYy04OWY2LWZiMzZmMDQ0OWNhMg.png",
-    photo_url: "http://www.ft.com/fastft/files/2017/05/sophos-attack.png",
-    humanized_time_ago: humanized_time_ago(180),
-    like_count: 0,
-    comment_count: 2,
-    comments: [{ 
-        username: "Sophos_2017",
-        text:  "Stocks going up, weehoooo",
-        }]
-    }
-    @posts = [@post_WannaCry, @post_ABT, @post_Sophos]
-    
+    @posts = Post.order(created_at: :desc)
+       
     erb(:index)
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,23 @@ class Post < ActiveRecord::Base
     has_many :comments
     has_many :likes
     
+    def humanized_time_ago
+        time_ago_in_seconds = Time.now - self.created_at
+        time_ago_in_minutes = time_ago_in_seconds / 60
+        
+        if time_ago_in_minutes >= 60
+            "#{(time_ago_in_minutes / 60).to_i}h"
+        else
+            "#{time_ago_in_minutes.to_i}m"
+        end
+    end
+    
+    def like_count
+        self.likes.size
+    end
+    
+    def comment_count
+        self.comments.size
+    end
+    
 end
