9ae3a87937f17cb05c4952fc10883b45a1d54e06
James De Grano <jamesdegrano1@gmail.com>
2017-06-09 00:21:44 +0000

    V1.61 updated form submission for better looks connected to database

--- app/actions.rb
+++ app/actions.rb
@@ -10,3 +10,16 @@ get '/signup' do
     @user = User.new
     erb(:signup)
 end
+
+post '/signup' do
+    
+    email       = params[:email]
+    avatar_url  = params[:avatar_url]
+    username    = params[:username]
+    password    = params[:password]
+    
+    user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+    user.save
+    
+    escape_html user.inspect
+end
