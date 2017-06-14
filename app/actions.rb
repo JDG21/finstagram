d133c651c4137f5993197d1671781e2d68926814
James De Grano <jamesdegrano1@gmail.com>
2017-06-14 02:18:29 +0000

    v1.71 added logout and session for users

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,15 @@
 
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    "Logout Successful!"
+end
+
 get '/login' do
     erb(:login)
 end
@@ -10,12 +21,13 @@ post '/login' do
     user = User.find_by(username: username)
     
     if user && user.password == password
-        
-        "Success"
+        session[:user_id] = user.id
+        "Success! User with id #{session[:user_id]} is logged in!"
             
         else
             
-        "Login Failed"
+        @error_message = "Login Failed"
+        erb(:login)
       
     end
     
@@ -23,7 +35,6 @@ end
 
 get '/' do
     @posts = Post.order(created_at: :desc)
-       
     erb(:index)
 end
 
